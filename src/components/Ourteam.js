419092617d3dfe5e931490c48b04d04f2b1ffa8d
ll00lll00l0l <ll00lll00l0l@gmail.com>
2024-02-19 13:30:38 +0530

    compare ui with site

--- src/components/Ourteam.js
+++ src/components/Ourteam.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Team1 from '../assets/img/ourteam1.png';
+import Team2 from '../assets/img/ourteam2.jpg';
+
 import Coma  from './Allsvgcomp.js'; 
 import  './Ourteam.css'; 
 
@@ -12,19 +14,19 @@ function ourteam() {
         <h1>Our Team</h1>
     
           <div>
-          <img src={Team1} alt='' className='img-fuild' />
+          <img src={Team2} alt='' className='img-fuild' />
            <div><h5>Ummer Kongan</h5>
          <h6>Founder & Managing Director, Thara Group</h6></div>
           </div>
           <div>
-          <img src={Team1} alt='' className='img-fuild'/>
-          <div><h5>Naji Kongan</h5>
-          <h6>Founder & Executive Director, Thara Group</h6></div>
-          </div>
-          <div>
           <img src={Team1} alt=''className='img-fuild'/>
           <div><h5>Nadeem Kongan</h5>
-          <h6>Marketing & Executive Director, Thara Group</h6></div>
+          <h6> CEO & Executive Director, Thara Group</h6></div>
+          </div>
+          <div>
+          <img src={Team1} alt='' className='img-fuild'/>
+          <div><h5>Naji Kongan</h5>
+          <h6>Executive Director, Thara Group</h6></div>
           </div>
         </div>
         <div className='teamcol2 col-md-6'>
